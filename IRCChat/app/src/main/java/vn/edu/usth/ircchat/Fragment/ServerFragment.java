1d3a9fd9a30de299bc91a4457ad08fa55dc3fcd5
Huong Thao <ththao111@gmail.com>
2016-12-03 23:43:53 +0700

    Make server button change color when click

--- IRCChat/app/src/main/java/vn/edu/usth/ircchat/Fragment/ServerFragment.java
+++ IRCChat/app/src/main/java/vn/edu/usth/ircchat/Fragment/ServerFragment.java
@@ -4,10 +4,15 @@ package vn.edu.usth.ircchat.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 import vn.edu.usth.ircchat.ListMyServerActivity;
 import vn.edu.usth.ircchat.R;
@@ -21,8 +26,22 @@ public class ServerFragment extends Fragment {
     public ServerFragment() {
         // Required empty public constructor
     }
+//    public static ServerFragment newInstance(ArrayList<String> my_servers) {
+//        ServerFragment serverFragment = new ServerFragment();
+//        Bundle args = new Bundle();
+//        args.putStringArrayList("myServers",my_servers);
+//        serverFragment.setArguments(args);
+//        return serverFragment;
+//    }
 
-
+//    public void createButton(ArrayList<String> myServers) {
+//        LinearLayout linearLayout = (LinearLayout) getView().findViewById(R.id.list_my_server);
+//        for (int i=0; i<myServers.size(); i++){
+//            Button button = new Button(this.getContext());
+//            button.setText(myServers.get(i));
+//            linearLayout.addView(button);
+//        }
+//    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,5 +49,10 @@ public class ServerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_server, container, false);
     }
 
-
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        ArrayList<String> myServers = getArguments().getStringArrayList("myServers");
+//        createButton(myServers);
+//    }
 }
